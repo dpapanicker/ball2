38e005b775cfee34109a10c3654dcd888ff53113
vititewari <70894215+vititewari@users.noreply.github.com>
2020-10-12 17:24:21 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,36 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=70
+		this.body=Bodies.circle(this.x, this.y, 70, options)
+		World.add(world, this.body);
+		
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,70, 70);
+			pop()
+			
+	}
+
+}
