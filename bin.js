f0e76a2fa0450d9ed5670146cf9498a2847db4ba
vititewari <70894215+vititewari@users.noreply.github.com>
2020-10-13 14:36:34 +0530

    Update bin.js

--- bin.js
+++ bin.js
@@ -4,65 +4,43 @@ class dustbin
 	{
 		this.x=x;
 		this.y=y;
-		this.dustbinWidth=200;
-		this.dustbinHeight=100;
-		this.wallThickness=20;
-		this.angle=0;	
-		this.image=loadImage("dustbingreen.png")
-
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		Matter.Body.setAngle(this.leftWallBody, this.angle);
 		
+			this.dustbinWidth=200;
+ 		this.dustbinHeight=213;
+ 		this.wallThickness=20;
+ 		this.angle=-0.25;
+ 		this.offset=20	
+ 		this.image=loadImage("trashcangreen.png")
 
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
-		World.add(world, this.bottomBody)
-		World.add(world, this.leftWallBody)
-		World.add(world, this.rightWallBody);
-		
-	}
-	display()
-	{
-			var posBottom=this.bottomBody.position;
-			var posLeft=this.leftWallBody.position;
-			var posRight=this.rightWallBody.position;
+ 		this.image=loadImage("dustbingreen.png")
+ 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+ 		this.leftWallBody=Bodies.rectangle(this.x+this.offset-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+ 		Matter.Body.setAngle(this.leftWallBody, this.angle);
+ 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 
-			this.image = loadImage("sprites/dustbingreen.png")
 
-			push()
-			translate(posLeft.x, posLeft.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			stroke(255)
-			rotate(this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
 
-			push()
-			translate(posRight.x, posRight.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			stroke(255)
-			angleMode(RADIANS)
-			fill(255)
-			rotate(-1*this.angle)
-			rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
+ 		this.rightWallBody=Bodies.rectangle(this.x-this.offset+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+ 		Matter.Body.setAngle(this.rightWallBody, -1*this.angle);
+ 		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 
-			push()
-			translate(posBottom.x, posBottom.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			stroke(255)
-			angleMode(RADIANS)
-			imageMode(CENTER);
+ 		World.add(world, this.bottomBody)
+ 		World.add(world, this.leftWallBody)
+ 		World.add(world, this.rightWallBody);
+ @@ -51,11 +50,11 @@ class dustbin
+ 			pop()
+
+ 			push()
+ 			translate(posBottom.x, posBottom.y);
+ 			translate(posBottom.x, posBottom.y+10);
+ 			rectMode(CENTER)
+ 			//strokeWeight(4);
+ 			angleMode(RADIANS)
+ 			//fill(255)
+ 			fill(255)
+ 			imageMode(CENTER);
  			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
-			fill(255)
-			rect(0,0,this.dustbinWidth, this.wallThickness);
-			pop()
+ 			//rect(0,0,this.dustbinWidth, this.wallThickness);
 			
 	}
 
